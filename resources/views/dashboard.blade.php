9d723fef31be6d43e2575f0c2ce6d2f979123178
maria geni anita mare <nonamare80@gmail.com>
2026-07-20 17:21:18 +0700

    Menambahkan layout dashboard admin, kasir, dan pelanggan

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,26 +1,84 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <title>Admin Dashboard</title>
+@extends('layouts.admin')
 
-    @vite(['resources/css/app.css','resources/js/app.js'])
-</head>
+@section('content')
 
-<body>
+<div class="row">
 
-<div class="container mt-5">
+    <div class="col-md-3 mb-4">
 
-<h1>Dashboard Admin</h1>
+        <div class="card card-dashboard p-4">
 
-<hr>
+            <div class="icon-card">
+                <i class="fa fa-list"></i>
+            </div>
 
-<h3>Selamat Datang Admin 👋</h3>
+            <h5 class="mt-3">Kategori</h5>
 
-<p>
-Kelola Menu, Kategori, Pesanan dan Laporan.
-</p>
+            <h2>0</h2>
+
+        </div>
+
+    </div>
+
+    <div class="col-md-3 mb-4">
+
+        <div class="card card-dashboard p-4">
+
+            <div class="icon-card">
+                <i class="fa fa-utensils"></i>
+            </div>
+
+            <h5 class="mt-3">Menu</h5>
+
+            <h2>0</h2>
+
+        </div>
+
+    </div>
+
+    <div class="col-md-3 mb-4">
+
+        <div class="card card-dashboard p-4">
+
+            <div class="icon-card">
+                <i class="fa fa-cart-shopping"></i>
+            </div>
+
+            <h5 class="mt-3">Pesanan</h5>
+
+            <h2>0</h2>
+
+        </div>
+
+    </div>
+
+    <div class="col-md-3 mb-4">
+
+        <div class="card card-dashboard p-4">
+
+            <div class="icon-card">
+                <i class="fa fa-users"></i>
+            </div>
+
+            <h5 class="mt-3">Pelanggan</h5>
+
+            <h2>0</h2>
+
+        </div>
+
+    </div>
 
 </div>
 
-</body>
-</html>
+<div class="card card-dashboard p-4">
+
+    <h4>Selamat Datang 👋</h4>
+
+    <p>
+        Selamat datang di Sistem Penjualan Kantin
+        <strong>Bakso Pakde Heru</strong>.
+    </p>
+
+</div>
+
+@endsection
